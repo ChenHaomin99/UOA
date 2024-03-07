fbeadef52ed2cee6f2628f7a5721705075a71642
Haomin <1029383633@qq.com>
2024-03-07 17:44:43 +1030

    test

---
+++ question14.sh
@@ -0,0 +1,8 @@
+# Fetch the latest changes from the remote repository
+git fetch
+
+# Switch to the 'update1' branch
+git checkout update1
+
+# Pull the latest changes for the 'update1' branch
+git pull origin update1
---
+++ question8.sh
@@ -0,0 +1,11 @@
+# Create the directory dir2
+mkdir dir2
+
+# Move all text files to dir2
+mv *.txt dir2/
+
+# Stage the changes
+git add -A
+
+# Commit the changes with a commit message
+git commit -m "Move all text files to dir2"
