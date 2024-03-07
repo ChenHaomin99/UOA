070c5b5b2cd446190ad42ae2eb6aefe518b7764f
ChenHaomin99 <147612988+ChenHaomin99@users.noreply.github.com>
2024-03-07 17:10:01 +1030

    Add files via upload

---
+++ Assignment/question10.sh
@@ -0,0 +1,5 @@
+# Switch to the main branch
+git checkout main
+
+# Merge branch1 into the main branch
+git merge branch1
---
+++ Assignment/question12.sh
@@ -0,0 +1,7 @@
+# Switch back to branch2
+git checkout branch2
+
+# Restore and commit uncommitted changes
+git restore file4
+git add file4
+git commit -m "Restore and commit changes in file4"
