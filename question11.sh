893f15bfb7096eb2d7db736954d0390e2246fc6f
Haomin <1029383633@qq.com>
2024-03-08 16:37:07 +1030

    ..

--- question11.sh
+++ question11.sh
@@ -1,8 +1,5 @@
-# Create a new branch named branch2
-git branch branch2
 
-# Switch to the newly created branch
-git checkout branch2
+git checkout -b branch2
 
 
 # Create and commit a file file4
@@ -10,8 +7,9 @@ touch file4
 git add file4
 git commit -m "Add file4 in branch2"
 
-# Modify file4
+
 echo "hi" > file4
 
-# Switch back to the main branch without committing changes
+git stash
+
 git checkout main
--- question12.sh
+++ question12.sh
@@ -1,7 +1,7 @@
 # Switch back to branch2
 git checkout branch2
 
-# Restore and commit uncommitted changes
+git stash pop
 
 git add file4
 git commit -m "Restore and commit changes in file4"
