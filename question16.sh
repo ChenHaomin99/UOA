3a740cf63cd82050d1d9050ef3b60e1c466fe40a
Haomin <1029383633@qq.com>
2024-03-08 18:23:28 +1030

    1

--- question16.sh
+++ question16.sh
@@ -1,6 +1,6 @@
 
 git branch -r
-git checkout -b loc2 origin/branch2
-git checkout -b loc3 orogin/branch3
-git merge origin/branch2
-git branch -D loc3
+git checkout branch2
+git checkout branch3
+git merge branch2
+git branch -D branch3
